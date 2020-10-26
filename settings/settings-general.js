6528530752539defa32d8ba53e08be5dedb2d556
csabourin <christian.sabourin@canada.ca>
2020-10-26 10:19:17 -0400

    Initial commit

---
+++ settings/settings-general.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+define({
+	"courseLegacyCode":  "I010",
+	"courseTitle_en": "GCDOCS Fundamentals",
+	"courseTitle_fr": "Cours de base sur GCDOCS",
+	"courseSubtitle_en": "Online Self-paced",
+	"courseSubtitle_fr": "En ligne - à rythme libre",
+	"seriesTitle_en": "",
+	"seriesTitle_fr": "",
+	
+	"showHelp": 	  true,
+	"showSitemap":	  false,
+	"showToolbox": 	  true,
+	"showFavorites":  false,
+	"showGlossary":   true,
+	"showResources":  true,
+	"addToolboxPage": [],
+
+	"trackAllPages": true,
+	"triggerCompletionWhenAllPagesViewed": false,
+	
+	"googleAnalyticsID": "UA-101907595-9",
+  	"editMode": true
+});
